97addd78f805b59959032ece3ffbce20cdc53699
Sheldon <sheldonaldridge@yahoo.com>
2022-05-02 23:04:01 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -15,3 +15,118 @@ let updateTimer = setInterval(function(){
     
 }, 1000)
 
+
+/******************************************
+ * getting the list of the nav item class *              
+ ******************************************/
+
+let getNav = document.querySelector(".home");
+
+/*************************
+ * Home tab hover effect *              
+ *************************/
+
+let displayAgumon = document.querySelector(".agumon");
+
+getNav.addEventListener('mouseover', function handleMouseOver(){
+
+    if(getNav){
+    displayAgumon.style.display = "block";
+    }
+
+})
+
+getNav.addEventListener('mouseout', function handleMouseOut(){
+
+    if(getNav){
+    displayAgumon.style.display = "none";
+    }
+
+})
+
+/*****************************
+ * About me tab hover effect *              
+ *****************************/
+let getaboutme = document.querySelector(".aboutme");
+
+let displayAgumonPower = document.querySelector(".agumon-spit-fire");
+
+getaboutme.addEventListener('mouseover', function handleMouseOver(){
+
+    if(getaboutme){
+        displayAgumonPower.style.display = "block";
+    }
+
+})
+
+getaboutme.addEventListener('mouseout', function handleMouseOut(){
+
+    if(getaboutme){
+        displayAgumonPower.style.display = "none";
+    }
+
+})
+
+/*******************************
+ * Experience tab hover effect *              
+ *******************************/
+
+ let getExperience = document.querySelector(".experience");
+
+ let displayAgumonPowerball = document.querySelector(".agumon-powerball-fire");
+ 
+ getExperience.addEventListener('mouseover', function handleMouseOver(){
+ 
+     if(getExperience){
+        displayAgumonPowerball.style.display = "block";
+     }
+ 
+ })
+ 
+ getExperience.addEventListener('mouseout', function handleMouseOut(){
+ 
+     if(getExperience){
+        displayAgumonPowerball.style.display = "none";
+     }
+ 
+ })
+
+ /************************
+ * Work tab hover effect *              
+ *************************/
+
+  let getwork = document.querySelector(".work")
+
+  let displayAgumonJump = document.querySelector(".agumon-jumping")
+
+  getwork.addEventListener('mouseover', function handleMouseOut(){
+      if(getwork){
+        displayAgumonJump.style.display = "block";
+      }
+  })
+
+  getwork.addEventListener('mouseout', function handleMouseOut(){
+    if(getwork){
+      displayAgumonJump.style.display = "none";
+    }
+})
+
+ /***************************
+ * Contact tab hover effect *              
+ ****************************/
+
+  let getcontact = document.querySelector(".contact")
+
+  let displayAgumonRun = document.querySelector(".agumon-running")
+
+  getcontact.addEventListener('mouseover', function handleMouseOut(){
+      if(getcontact){
+        displayAgumonRun.style.display = "block";
+      }
+  })
+
+  getcontact.addEventListener('mouseout', function handleMouseOut(){
+    if(getcontact){
+        displayAgumonRun.style.display = "none";
+    }
+})
