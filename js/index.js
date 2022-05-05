2e0254e30eb0f61d82b6d329b48a02c9e1a47d9e
Sheldon <sheldonaldridge@yahoo.com>
2022-05-05 13:44:58 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,3 @@
-let countDown = new Date("Sep 01, 2022 15:01:00").getTime()
-
-
-let updateTimer = setInterval(function(){
-
-    let now = new Date().getTime()
-    let timeLeft = countDown - now;
-
-    let days = Math.floor(timeLeft / (1000 * 60 * 60* 24));
-    let hours = Math.floor((timeLeft % (1000 * 60 * 60* 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-
-    let timer = document.querySelector('.count-down').innerHTML = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`
-    
-}, 1000)
-
 
 /******************************************
  * getting the list of the nav item class *              
