31441fa451c4d4c23357a0816336d41b2dd1bd1f
Sheldon <sheldonaldridge@yahoo.com>
2022-05-15 01:31:06 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 let projects = document.querySelector(".projects");
+let projectList = document.querySelector(".project-list")
+let clickOff = document.querySelector(".img-fluid")
 
-projects.addEventListener()
+projects.addEventListener('click', function handleMouseOver(){
+    if(projects){
+        projectList.style.display = "block";
+    }
+})
+
+clickOff.addEventListener('click', function handleClickOff(){
+    if(clickOff){
+        projectList.style.display = "none"
+    }
+})
