3361329ef9ad491dc3fa8d007abe6a251a84664b
Sheldon <sheldonaldridge@yahoo.com>
2022-06-24 12:22:08 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,28 +89,4 @@ staffedUp.addEventListener('click', function openCCard(){
     }
 })
 
-//Skillset SlideShow
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-//next and back slide show control
-function plusSlides(n){
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n){
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.querySelector(".slides");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  slides[slideIndex - 1].style.display = "block";
-}
